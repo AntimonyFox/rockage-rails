839cb4a615d2213a1119ef325cdbea342506f709
MechaFox <aeseligman@gmail.com>
2015-02-07 12:29:49 -0800

    Add num signed up

--- app/serializers/tournament_serializer.rb
+++ app/serializers/tournament_serializer.rb
@@ -1,9 +1,13 @@
 class TournamentSerializer < ActiveModel::Serializer
-  attributes :name, :slug, :max_num_entries, :spaces_available
+  attributes :name, :slug, :max_num_entries, :spaces_available, :num_sign_ups
 
   has_one :bracket
 
   def spaces_available
     max_num_entries - Tournament.find_by_slug(slug).users.count
+  end
+
+  def num_sign_ups
+    Tournament.find_by_slug(slug).users.count
   end
 end
