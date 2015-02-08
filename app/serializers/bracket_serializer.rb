e42517b75a0130d19a3b4b2efd3c3374159565f6
MechaFox <aeseligman@gmail.com>
2015-02-07 20:04:15 -0800

    Dash serializer

--- app/serializers/bracket_serializer.rb
+++ app/serializers/bracket_serializer.rb
@@ -4,4 +4,13 @@ class BracketSerializer < ActiveModel::Serializer
   has_one :user
   has_many :brackets
 
+  def user
+    t = Tournament.find(tournament_id)
+    if t.slug == "dash"
+      return user.username + " & " + partner.username
+    else
+      return user.username
+    end
+  end
+
 end
